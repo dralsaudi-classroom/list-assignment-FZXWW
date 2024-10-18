2407156c98e39d7c5669e207220bb28cd83b0dd5
Mohmmad Alqurashi <dr.fmfm2004@gmail.com>
2024-10-18 18:47:55 +0300

    Update ListTester.java

--- src/main/java/com/example/project/ListTester.java
+++ src/main/java/com/example/project/ListTester.java
@@ -1,9 +1,10 @@
 package com.example.project;
 
 public class ListTester {
+
+    
     public static <T> void circularLeftShift(List <T> list, int n)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
 
         for(int i = 0 ; i < n ; i++){
             list.findFirst();
@@ -19,12 +20,24 @@ public class ListTester {
     }
     public static <T> void reverseCopy(DLL<T> l1, DLL<T> l2)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
-        // Write the method reverseCopy, user of DoubleLinkedList, which copies the elements
+         // Write the method reverseCopy, user of DoubleLinkedList, which copies the elements
         // of l1 to l2 in reverse order. The list l1 must not change. Assume that l2 is empty.
         // The method signature is public static <T> void reverseCopy(DoubleLinkedList<T> l1,
         // DoubleLinkedList<T> l2).
         // Example 4.1. If l1 : A ↔ B ↔ C ↔ D, then calling reverseCopy(l1, l2) results in
         // l2 : D ↔ C ↔ B ↔ A.
-    }
+
+        l.findFirst();
+        while(!l1.last()){
+
+            while( !l1.last() ) l.findNext();
+            T x = l1.retrieve();
+            l2.insert(x);
+        }
+         T x = l1.retrieve();
+            l2.insert(x);
+        
+   }
+
+    
 }
